206f6debc0c9e8133c89f06b0011a4db851b67eb
chenbinbin <1056477503@qq.com>
2018-02-22 20:29:03 +0800

    模板方法模式

---
+++ src/main/java/behavior/templateMethod/TemplateMethodTest.java
@@ -0,0 +1,13 @@
+package behavior.templateMethod;
+
+public class TemplateMethodTest {
+    public static void main(String[] args) {
+        BankTemplateMethod method = new BankTemplateMethod() {
+            @Override
+            protected void handleBusiness() {
+                System.out.println("办理存款业务！");
+            }
+        };
+        method.process();
+    }
+}
