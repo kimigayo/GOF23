27b95143c564bb66603e59755f090af96610f4d2
chenbinbin <1056477503@qq.com>
2018-02-22 21:17:04 +0800

    观察者模式

---
+++ src/main/java/behavior/observer/ConcreteSubject.java
@@ -0,0 +1,14 @@
+package behavior.observer;
+
+public class ConcreteSubject extends Subject{
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyObserver();
+    }
+}
