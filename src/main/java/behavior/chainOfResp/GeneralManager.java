13b8b6bbf0b4918ef7aadfb3f5b598a4f56def21
chenbinbin <1056477503@qq.com>
2018-02-22 15:56:02 +0800

    责任链模式

---
+++ src/main/java/behavior/chainOfResp/GeneralManager.java
@@ -0,0 +1,17 @@
+package behavior.chainOfResp;
+
+public class GeneralManager extends Leader{
+    @Override
+    public void handlerRequest(LeaveRequest request) {
+
+        if(request.getLeaveDay()<6){
+            System.out.println(super.name+":處理三天以下請假申請--");
+        }else {
+            super.nextLeader.handlerRequest(request);
+        }
+    }
+
+    public GeneralManager(String name) {
+        super(name);
+    }
+}
