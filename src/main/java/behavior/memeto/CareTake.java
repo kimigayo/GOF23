4f247efad54abdd04c9af7bd43d6f816aa0dce3c
chenbinbin <1056477503@qq.com>
2018-02-22 21:36:02 +0800

    备忘录模式

---
+++ src/main/java/behavior/memeto/CareTake.java
@@ -0,0 +1,13 @@
+package behavior.memeto;
+
+public class CareTake {
+    private EmpMemeto empMemeto;
+
+    public EmpMemeto getEmpMemeto() {
+        return empMemeto;
+    }
+
+    public void setEmpMemeto(EmpMemeto empMemeto) {
+        this.empMemeto = empMemeto;
+    }
+}
